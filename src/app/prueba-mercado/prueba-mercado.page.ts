782d413ec6ac4ab453640bec7d22b368dac23274
Brayan Farid Garcia Garzon <bgarcia@MacBook-Air-de-Brayan.local>
2024-10-15 22:48:46 -0500

    Se ajusto listas

--- src/app/prueba-mercado/prueba-mercado.page.ts
+++ src/app/prueba-mercado/prueba-mercado.page.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar, IonSearchbar, IonList, IonItem, IonLabel } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonList, IonItem, IonLabel, IonSelect, IonSelectOption } from '@ionic/angular/standalone';
 import { MercadoLibre2Service } from '../services/mercado-libre2.service';
 
 @Component({
@@ -9,50 +9,44 @@ import { MercadoLibre2Service } from '../services/mercado-libre2.service';
   templateUrl: './prueba-mercado.page.html',
   styleUrls: ['./prueba-mercado.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, IonSearchbar, IonList, IonItem, IonLabel, CommonModule, FormsModule]
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, IonList, IonItem, IonLabel, IonSelect, IonSelectOption, CommonModule, FormsModule]
 })
 export class PruebaMercadoPage implements OnInit {
-  categorias: any[] = [];
-  resultadosBusqueda: any[] = [];
+  marcas: string[] = [];
+  modelos: string[] = [];
+  marcaSeleccionada: string = '';
+  modeloSeleccionado: string = '';
 
   constructor(private mercadoLibreService: MercadoLibre2Service) { }
 
   ngOnInit() {
-    this.cargarCategorias();
+    this.cargarMarcas();
   }
 
-  cargarCategorias() {
-    this.mercadoLibreService.getCategorias().subscribe(
-      (data) => {
-        this.categorias = data.filter((cat: any) => cat.id === 'MCO1743')[0].children_categories;
+  cargarMarcas() {
+    this.mercadoLibreService.getMarcas().subscribe(
+      (marcas) => {
+        this.marcas = marcas;
       },
       (error) => {
-        console.error('Error al cargar categorías:', error);
+        console.error('Error al cargar marcas:', error);
       }
     );
   }
 
-  buscarVehiculos(event: any) {
-    const query = event.target.value.toLowerCase();
-    if (query && query.length > 2) {
-      this.mercadoLibreService.buscarVehiculos(query).subscribe(
-        (data) => {
-          const resultadosUnicos = new Map<string, any>();
-          data.results.forEach((item: any) => {
-            const [marca, modelo] = item.title.split(' ', 2);
-            const key = `${marca} ${modelo}`.toLowerCase();
-            if (!resultadosUnicos.has(key)) {
-              resultadosUnicos.set(key, { marca, modelo });
-            }
-          });
-          this.resultadosBusqueda = Array.from(resultadosUnicos.values());
+  onMarcaChange() {
+    if (this.marcaSeleccionada) {
+      this.mercadoLibreService.getModelos(this.marcaSeleccionada).subscribe(
+        (modelos) => {
+          this.modelos = modelos;
         },
         (error) => {
-          console.error('Error en la búsqueda:', error);
+          console.error('Error al cargar modelos:', error);
         }
       );
     } else {
-      this.resultadosBusqueda = [];
+      this.modelos = [];
     }
+    this.modeloSeleccionado = '';
   }
 }
--- src/app/services/mercado-libre2.service.ts
+++ src/app/services/mercado-libre2.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -16,5 +17,31 @@ export class MercadoLibre2Service {
 
   buscarVehiculos(query: string): Observable<any> {
     return this.http.get(`${this.apiUrl}/sites/MCO/search?category=MCO1743&q=${query}`);
+  }
+
+  getMarcas(): Observable<string[]> {
+    return this.buscarVehiculos('').pipe(
+      map(data => {
+        const marcasSet = new Set<string>();
+        data.results.forEach((item: any) => {
+          const marca = item.title.split(' ')[0];
+          marcasSet.add(marca);
+        });
+        return Array.from(marcasSet).sort();
+      })
+    );
+  }
+
+  getModelos(marca: string): Observable<string[]> {
+    return this.buscarVehiculos(marca).pipe(
+      map(data => {
+        const modelosSet = new Set<string>();
+        data.results.forEach((item: any) => {
+          const [, modelo] = item.title.split(' ', 2);
+          if (modelo) modelosSet.add(modelo);
+        });
+        return Array.from(modelosSet).sort();
+      })
+    );
   }
 }
