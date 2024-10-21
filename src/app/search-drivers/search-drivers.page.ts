00196b9ff2dd7068ad0fe69b8b90b5015c5ba727
Brayan Farid Garcia Garzon <bgarcia@MacBook-Air-de-Brayan.local>
2024-10-21 17:54:08 -0500

    Se agrego sliders

--- src/app/search-drivers/search-drivers.page.ts
+++ src/app/search-drivers/search-drivers.page.ts
@@ -1,16 +1,20 @@
-import { Component, OnInit, ViewChild, AfterViewInit } from '@angular/core';
+import { Component, OnInit, ViewChild, AfterViewInit, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonContent, IonHeader, IonTitle, IonToolbar, IonFabButton, IonList, IonItem, IonLabel, IonProgressBar, IonButton, IonFab, IonIcon } from '@ionic/angular/standalone';
 import { Router } from '@angular/router';
 import { MapaComponent } from '../mapa/mapa.component';
 
+// Importa el registro de Swiper
+import { register } from 'swiper/element/bundle';
+
 @Component({
   selector: 'app-search-drivers',
   templateUrl: './search-drivers.page.html',
   styleUrls: ['./search-drivers.page.scss'],
   standalone: true,
-  imports: [IonIcon, IonFab, IonFabButton, IonButton, IonProgressBar, IonContent, MapaComponent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonList, IonItem, IonLabel]
+  imports: [IonIcon, IonFab, IonFabButton, IonButton, IonProgressBar, IonContent, MapaComponent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonList, IonItem, IonLabel],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA] // Necesario para usar elementos personalizados como swiper
 })
 export class SearchDriversPage implements OnInit, AfterViewInit {
   @ViewChild(MapaComponent) mapaComponent!: MapaComponent;
@@ -21,6 +25,9 @@ export class SearchDriversPage implements OnInit, AfterViewInit {
   problemData: any;
 
   constructor(private router: Router) {
+    // Registra Swiper para usar elementos personalizados
+    register();
+
     const navigation = this.router.getCurrentNavigation();
     if (navigation?.extras.state) {
       this.userData = navigation.extras.state['userData'];
